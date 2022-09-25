f1abfea97dae362c0012e170bc44d88fb9a261c8
acct.1 <acct.1@acct1s-MacBook-Pro.local>
2022-09-24 22:23:27 -0700

    Finishing Mod 7

--- Queries.sql
+++ Queries.sql
@@ -100,3 +100,75 @@ left join dept_emp as de
 on ce.emp_no = de.emp_no
 group by de.dept_no
 order by de.dept_no;
+
+--Creating employee information table--
+select e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+into emp_info
+from employee as e
+inner join salary as s
+on (e.emp_no = s.emp_no)
+inner join dept_emp as de
+on (e.emp_no = de.emp_no)
+where (e.birth_date between '1952-01-01' and '1955-12-31')
+and (e.hire_date between '1985-01-01' and '1988-12-31')
+and (de.to_date = '9999-01-01');
+
+--List of managers per department--
+select dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+into manager_info
+from dept_manager as dm
+inner join department as d
+on (dm.dept_no = d.dept_no)
+inner join current_emp as ce
+on (dm.emp_no = ce.emp_no);
+
+--Creating the dept_info table--
+select ce.emp_no,
+    ce.first_name,
+    ce.last_name,
+    d.dept_name
+into dept_info
+from current_emp as ce
+inner join dept_emp as de
+on (ce.emp_no = de.emp_no)
+inner join department as d
+on (de.dept_no = d.dept_no);
+
+--Creating a taliored list--
+select ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+    d.dept_name
+--into sales_list
+from retirement_info as ri
+inner join dept_emp as de
+on (ri.emp_no = de.emp_no)
+inner join department as d
+on (de.dept_no = d.dept_no)
+where (d.dept_name = 'Sales')
+order by d.dept_name;
+
+
+select ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+    d.dept_name
+--into sales_and_development_list
+from retirement_info as ri
+inner join dept_emp as de
+on (ri.emp_no = de.emp_no)
+inner join department as d
+on (de.dept_no = d.dept_no)
+where d.dept_name in ('Sales', 'Development')
+
